bc43b2c50167b336586cf9b5975e3357f731448d
TienNM <tiennmse170204@fpt.edu.vn>
2024-10-07 12:59:37 +0700

    fix layout

--- Main/BackEnd/src/main/java/com/example/profile_api/config/JwtGeneratorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.profile_api.config;
-
-import com.example.profile_api.model.User;
-
-import java.util.Map;
-
-public interface JwtGeneratorInterface {
-        Map<String, String> generateToken(User user) throws Exception;
-}
